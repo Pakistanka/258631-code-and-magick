6c66609325c14aca14023d9d75fb766e2b0e7230
Anna <polozova.anastasi@gmail.com>
2018-09-13 04:43:54 +0300

    setup

---
+++ js/setup.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var persons = [];
+
+var firstPerson = {
+  name: chooseName + chooseSurname,
+
+  chooseName: function(name){
+    var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  },
+
+  chooseSurname: function(surname) {
+    var names = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  }
+}
